156ba9d33db2823eae355d8a52f69fbb126cdf8d
wrLegend <wrangler1024@163.com>
2019-07-01 11:55:11 +0800

    提交到本地仓库

---
+++ git-test/src/main/java/com/itheima/User.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+/**
+ * @author Jeffery
+ * @version 1.0
+ * @description
+ * @date 2019/7/1 11:45
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
